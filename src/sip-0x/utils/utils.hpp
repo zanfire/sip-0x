e95c9504fbc54ca055562c29046ce5e6a2d29fb1
Matteo Valdina <zanfire@live.com>
2015-04-20 07:39:24 +0200

    Added enable_protected_dtor
    Added utility class that implements ceate and protected dtor.

---
+++ src/sip-0x/utils/utils.hpp
@@ -0,0 +1,33 @@
+#if !defined(SIP0X_UTILS_UTILS_HPP__)
+#define SIP0X_UTILS_UTILS_HPP__
+
+#include <memory>
+
+namespace sip0x
+{
+  namespace utils
+  {
+    template<class T>
+    class enable_protected_dtor {
+      friend class deleter;
+      
+      class deleter {
+      public:
+        void operator()(T* ptr) { delete ptr; }
+      };
+
+    public:
+      
+      template<typename ... Args>
+      static std::shared_ptr<T> create(Args&... rest) {
+        return std::shared_ptr<T>(new T(rest...), enable_protected_dtor<T>::deleter());
+      }
+
+    protected:
+      enable_protected_dtor(void) {}
+      virtual ~enable_protected_dtor(void) {}
+    };
+  }
+}
+
+#endif // SIP0X_UTILS_UTILS_HPP__
